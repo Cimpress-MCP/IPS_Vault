9b6535975240eb1b369be4a3f1ab4c069b3e4206
Jon Charette <jcharette@cimpress.com>
2018-03-23 16:13:53 -0400

    elb's go in public, silly

--- components/vault-cluster/elb.tf
+++ components/vault-cluster/elb.tf
@@ -9,7 +9,7 @@ module "vault_elb" {
   vpc_id                      = "${var.vpc_id}"
   allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
 
-  subnet_ids = "${data.aws_subnet_ids.private.ids}"
+  subnet_ids = "${data.aws_subnet_ids.public.ids}"
 
   create_dns_entry = true
   hosted_zone_id   = "${data.aws_route53_zone.default.zone_id}"
