82eb78ba099b8837b4789ab0f82a5df772aab096
Jon Charette <jcharette@cimpress.com>
2018-03-27 15:52:42 -0400

    fixing outputs of vault_vpc

--- components/vault-vpc/outputs.tf
+++ components/vault-vpc/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = "$(module.vpc.vpc_id)"
+  value = "${module.vpc.vpc_id}"
 }
 
 output "vpc_public_subnets" {
-  value = "${module.vpc.public_subnets}"
+  value = "${module.vpc.public_subnets_str}"
 }
 
 output "vpc_private_subnets" {
-  value = "${module.vpc.private_subnets}"
+  value = "${module.vpc.private_subnets_str}"
 }
